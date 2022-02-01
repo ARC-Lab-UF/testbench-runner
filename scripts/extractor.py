00b7312ed265f6d746dde055371166fcbfbfdcdf
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-02-01 16:38:42 -0500

    Replaces os.path calls with Pathlib. Also, small edits to name mangling function and extract function.
    Pathlib is a better option with more modern features, and seemed to work as a drop-in replacement for os.path.
    Not thoroughly tested, but seems to work okay for now.

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -4,15 +4,18 @@
     Extracts student submissions from a 
     Canvas submissions.zip file.
 """
-import os
 import shutil
 from pathlib import Path
 from typing import List
 from zipfile import ZipFile
 
 
-def _format_student_names(student_list_file: str) -> List[str]:
-    """Format student names as they appear in their mangled submissions.zip directories."""
+def _get_mangled_names(student_list_file: str) -> List[str]:
+    """
+    Format student names as they appear in their mangled submissions.zip directories.
+    Canvas submissions.zip name format is `[middle name(s)]<lastname><firstname>`.
+    For example, the name "Albert Gator" becomes "gatoralbert".
+    """
     with open(student_list_file, "r") as f:
         names = f.readlines()
 
@@ -22,7 +25,8 @@ def _format_student_names(student_list_file: str) -> List[str]:
         separated_names = name.lower().split()
 
         # Formatting of submissions.zip simply puts first name in the back.
-        formatted_names.append("".join([*separated_names[1:], separated_names[0]]))
+        separated_names.append(separated_names.pop(0))
+        formatted_names.append("".join(separated_names))
 
     return formatted_names
 
@@ -48,37 +52,37 @@ def extract_submissions(
     """
 
     # Get list of formatted student namnes
-    formatted_names = _format_student_names(student_list_file)
+    FORMATTED_NAMES = _get_mangled_names(student_list_file)
 
     # Get list of all student submissions
     with ZipFile(submissions_zip_path, "r") as z:
-        subfile = z.namelist()
+        all_zip_filenames = z.namelist()
 
-    # Get cooresponding mangled submission .zip filenames from inside submissions.zip
-    zip_filenames = [
+    # Get mangled submission.zip filenames in students.txt
+    ZIP_FILENAMES = [
         zip_name 
-        for zip_name in subfile 
-        for name in formatted_names 
-        if name in zip_name
+        for zip_name in all_zip_filenames # Iterate through all filenames in submissions.zip
+        for name in FORMATTED_NAMES # Iterate through all formatted names from students.txt
+        if name in zip_name  # Only keep zip_name if the formatted name appears somewhere in it's filename.
     ]
-
+    
     # Create the unzipped Submissions directory, which will be used to store each students' source code.
-    SUBS_UNZIP_PATH = os.path.join("Submissions/", lab_filename)
-    Path(SUBS_UNZIP_PATH).mkdir(parents=True, exist_ok=True)
+    SUBS_UNZIP_PATH = Path("Submissions/", lab_filename)
+    SUBS_UNZIP_PATH.mkdir(parents=True, exist_ok=True)
 
     # Extract only the mangled names we selected into the new submission directory.
     with ZipFile(submissions_zip_path) as z:
-        z.extractall(SUBS_UNZIP_PATH, zip_filenames)
+        z.extractall(SUBS_UNZIP_PATH, ZIP_FILENAMES)
 
     # For each student submission subdir, extract contents and copy all VHDL files to the top.
-    for zip_filename in zip_filenames:
+    for zip_filename in ZIP_FILENAMES:
 
         # Create a directory for this student's submission.  # TODO this could just use the normal name.
-        STU_UNZIP_PATH = os.path.join(SUBS_UNZIP_PATH, zip_filename.split("_")[0])
-        Path(STU_UNZIP_PATH).mkdir(parents=True, exist_ok=True)
+        STU_UNZIP_PATH = Path(SUBS_UNZIP_PATH, zip_filename.split("_")[0])
+        STU_UNZIP_PATH.mkdir(parents=True, exist_ok=True)
 
         # Extract all of the submission's contents into the new directory.
-        with ZipFile(os.path.join(SUBS_UNZIP_PATH, zip_filename)) as z:
+        with ZipFile(Path(SUBS_UNZIP_PATH, zip_filename)) as z:
             z.extractall(STU_UNZIP_PATH)
 
         # Find all the VHDL files in this submission
@@ -95,15 +99,16 @@ def extract_submissions(
                 pass
 
     # Remove the old student submission .zip files
-    for zip_filename in zip_filenames:
-        os.remove(os.path.join(SUBS_UNZIP_PATH, zip_filename))
+    for zip_filename in ZIP_FILENAMES:
+        # Path.unlink() removes the file
+        Path(SUBS_UNZIP_PATH, zip_filename).unlink()
 
     # If user chooses, delete submissions.zip
     if delete_zip:
         try:
-            os.remove(submissions_zip_path)
+            Path(submissions_zip_path).unlink()  # Remove path
         except:
             print('An error occurred while attempting to delete "submissions.zip".')
 
     # Return this so the other functions can know where to look for these files. TODO just send the paths directly.
-    return formatted_names
+    return FORMATTED_NAMES
