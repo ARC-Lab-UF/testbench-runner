dfbac64e5877ef8f9721b46fe6bc79dd9fc0d774
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-03-23 00:52:47 -0400

    formatted file

--- scripts/student_data.py
+++ scripts/student_data.py
@@ -2,17 +2,24 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
+
 @dataclass
 class StudentData:
     # Normal name ("First Last")
     name: str
+
+    # Zipped submission filename
+    zipped_submission: str = ""
+
     # Unzipped submission directory
     submission_dir: Path | None = None
+
     # Path of each VHDL file for this student
     vhdl_files: List[Path] | None = None
+
     # Name that appears in canvas zip files ("lastfirstmiddle(s)")
     @property
     def mangled_name(self) -> str:
         separated_names = self.name.lower().split()
-        separated_names = reversed(separated_names) # Reverse first,last
+        separated_names = reversed(separated_names)  # Reverse first,last
         return "".join(separated_names)
