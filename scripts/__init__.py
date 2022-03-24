3c1efa8d9cf5133164091dac1dc5141944826d53
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-03-24 00:48:40 -0400

    dot syntax for imports

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,3 @@
-from student_data import StudentData
-from extractor import extract_submissions
-from generate_tcl import generate_tcl
+from .student_data import StudentData
+from .extractor import extract_submissions
+from .generate_tcl import generate_tcl
