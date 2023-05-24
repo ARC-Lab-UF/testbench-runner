403146c73a6a3122863f4c6e3bdacaccd147b4be
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2023-05-24 06:56:29 -0400

    attempted to explain how randLabList.py works. No one uses it though so it's not in desperate need of a rewrite.

--- randLabList.py
+++ randLabList.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Random number generator for student labs
+Given a student_list.txt file, randomly generates demo timeslots for a lab.
 
 @author: keethsmith
 """
@@ -17,30 +17,53 @@ import re
 import random
 from datetime import datetime, timedelta
 
-parser = argparse.ArgumentParser(description='automated random list generator for labs.')
-parser.add_argument('-t', '--time', default="4:05 PM", type=str, help='lab start time, format: HH:MM AM/PM. Please account for quiz time -- if quiz is 20 min and start is 3:00PM then put 3:20PM as start time', required=False)
-parser.add_argument('-d','--duration', default="10" , type=str, help='duration of slot in minutes. Default is 10', required=False)
-parser.add_argument('-f','--file',default="studentList.txt",type=str,help='file of student names, separated by new line', required=False)
+parser = argparse.ArgumentParser(
+    description="automated random list generator for labs."
+)
+parser.add_argument(
+    "-t",
+    "--time",
+    default="4:05 PM",
+    type=str,
+    help="lab start time, format: HH:MM AM/PM. Please account for quiz time -- if quiz is 20 min and start is 3:00PM then put 3:20PM as start time",
+    required=False,
+)
+parser.add_argument(
+    "-d",
+    "--duration",
+    default="10",
+    type=str,
+    help="duration of slot in minutes. Default is 10",
+    required=False,
+)
+parser.add_argument(
+    "-f",
+    "--file",
+    default="studentList.txt",
+    type=str,
+    help="file of student names, separated by new line",
+    required=False,
+)
 args = parser.parse_args()
 
-pTime = args.time.replace(" ","")
+pTime = args.time.replace(" ", "")
 startTime = datetime.strptime(pTime, "%I:%M%p")
 
-if(args.duration!=None):
+if args.duration != None:
     duration = int(args.duration)
 studentFile = None
-if(args.file!=None):
-    if(os.path.isfile(args.file)):
+if args.file != None:
+    if os.path.isfile(args.file):
         studentFile = args.file
     else:
-        print("Error: could not open "+str(args.file))
+        print("Error: could not open " + str(args.file))
         sys.exit(-1)
 
 
 with open(studentFile, "r") as f:
     studentList = f.read().splitlines()
 
-#shuffle the student list
+# shuffle the student list
 random.shuffle(studentList)
 
 
@@ -48,16 +71,11 @@ random.shuffle(studentList)
 chosen_ones = ["John Doe", "Jane Doe"]
 
 for chosen_one in chosen_ones:
-
     if chosen_one in studentList:
-
         studentList.remove(chosen_one)
-        studentList.insert(random.randint(5,len(studentList)), chosen_one)
-
-
+        studentList.insert(random.randint(5, len(studentList)), chosen_one)
 
 
 for student in studentList:
-
-    print(str(startTime.strftime("%I:%M %p"))+" - "+str(student))
+    print(str(startTime.strftime("%I:%M %p")) + " - " + str(student))
     startTime = startTime + timedelta(minutes=duration)
